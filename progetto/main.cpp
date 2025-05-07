8e8e77153f54eb1b8650b00130f941c720c44e19
Romanucci Giuseppe <romanuccig@pascal.local>
2025-05-07 10:40:12 +0200

    aggiunta del menu e switch

--- progetto/main.cpp
+++ progetto/main.cpp
@@ -2,8 +2,85 @@
 
 using namespace std;
 
+// menù
+
+void menu(){
+
+    cout<< "==== GESTIONE UNIVESITARIA ====" <<endl;
+    cout<< "0. Carica dati file" <<endl;
+    cout<< "1. Cerca corsi di uno studente (matricola)" <<endl;
+    cout<< "2. Cerca corsi di uno studente (cognome)" <<endl;
+    cout<< "3. Elenca studenti iscritti di un corso" <<endl;
+    cout<< "4. Stampa dati esami di un corso" <<endl;
+    cout<< "5. Numero di studenti per corso" <<endl;
+    cout<< "6. Numero di materie per corso" <<endl;
+    cout<< "7. Cerca materie per descrizione" <<endl;
+    cout<< "8. Inserisci un nuovo studente" <<endl;
+    cout<< "9. Salva i dati su file" <<endl;
+    cout<< "X. Esci" <<endl;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
+  char op;
+
+  menu();
+  cout<< "Scelta: ";
+  cin>>op
+
+  while (op != 'X'){
+
+    switch (op){
+
+        case '1':
+
+            break;
+
+        case '2':
+
+            break;
+
+        case '3':
+
+            break;
+
+        case '4':
+
+            break;
+
+        case '5':
+
+            break;
+
+        case '6':
+
+            break;
+
+        case '7':
+
+            break;
+
+        case '8':
+
+            break;
+
+        case '9':
+
+            break;
+
+
+
+    }
+
+
+
+    menu();
+    cout<< "Scelta: " ;
+    cin>>op;
+  }
+
+
+
+
 }
